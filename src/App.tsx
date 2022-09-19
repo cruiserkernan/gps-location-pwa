0f7c4dcb495de3bac6b4eaf03bf2c8af06d02dd3
Martin Hjalmarsson <martin.hjalmarsson@nethouse.se>
2022-09-19 09:50:17 +0200

    Initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useGeolocated } from 'react-geolocated';
 
 function App() {
+  const { coords, isGeolocationAvailable, isGeolocationEnabled } = useGeolocated({
+    positionOptions: {
+      enableHighAccuracy: true,
+    },
+    watchPosition: true,
+    onSuccess: (position) => console.log('Got position', position),
+  })
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Accuracy: {coords?.accuracy}</p>
+        <p>Latitude: {coords?.latitude}</p>
+        <p>Longitude: {coords?.longitude}</p>
+
       </header>
     </div>
   );
