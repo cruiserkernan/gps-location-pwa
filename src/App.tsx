1343df9229c7386f64d91e9dbd3b1eba09e0e7c4
Martin Hjalmarsson <martin.hjalmarsson@nethouse.se>
2022-09-20 20:25:06 +0200

    Maps working

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
 import React from 'react';
 import './App.css';
 import { useGeolocated } from 'react-geolocated';
-
+import { Circle, GoogleMap, Marker, useLoadScript } from "@react-google-maps/api"
+const mapsApiKey = "AIzaSyDtm9RKuYkzVuDTvTFeOes4feNo4Yrgz-s";
 function App() {
+  const [center, setCenter] = React.useState({ lat: 59.26, lng: 15.20 });
   const { coords } = useGeolocated({
     positionOptions: {
       enableHighAccuracy: true,
     },
     watchPosition: true,
-    onSuccess: (position) => console.log('Got position', position),
-  })
+    onSuccess: (position) => setCenter({ lat: position.coords.latitude, lng: position.coords.longitude }),
+  });
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: mapsApiKey
+  });
+
+
+
+  const a = [center];
+
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header">
         <p>Accuracy: {coords?.accuracy}</p>
         <p>Latitude: {coords?.latitude}</p>
         <p>Longitude: {coords?.longitude}</p>
+        {
+          isLoaded ? (
 
+            <GoogleMap
+              center={center}
+              zoom={10}
+              mapContainerStyle={{
+                height: "400px",
+                width: "800px"
+              }}
+            >
+              <>
+                <Circle center={center} radius={coords?.accuracy ?? 10} />
+                {
+                  a.map((item, index) => {
+                    return <Marker
+                      key={index}
+                      position={item}
+                    />
+                  })
+                }
+              </>
+            </GoogleMap>
+          ) : <p>Loading...</p>
+        }
       </header>
-    </div>
+    </div >
   );
 }
 
